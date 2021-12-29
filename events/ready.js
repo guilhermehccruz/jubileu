b3eb8cbba2cbcfc7aa80c537d62373188f0a7b6e
guilhermehccruz <guihccruz60@gmail.com>
2021-12-29 20:53:24 -0300

    updated ready message

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,26 @@
 const { readyChannelId } = require('../config.json');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	name: 'ready',
 	once: true,
 	async execute(client) {
-		await client.channels.cache.get(readyChannelId).send('Pai ta on');
+		const dateObj = new Date();
+
+		const hour = ('0' + dateObj.getHours()).slice(-2);
+		const minute = ('0' + dateObj.getMinutes()).slice(-2);
+		const second = ('0' + dateObj.getSeconds()).slice(-2);
+
+		const day = dateObj.getDate();
+		const month = dateObj.getMonth();
+		const year = dateObj.getFullYear();
+
+		const fullDate = `${hour}:${minute}:${second} - ${day}/${month}/${year}`;
+
+		const embed = new MessageEmbed()
+			.setTitle('Pai ta on')
+			.setDescription(fullDate);
+
+		await client.channels.cache.get(readyChannelId).send(embed);
 	},
 };
