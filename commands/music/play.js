0b115dfd441e545aeea2f698d94a735757daa9ab
guilherme.cruz <71985319+guilhermehccruz@users.noreply.github.com>
2022-02-14 01:03:33 -0300

    implemented ytsr

--- commands/music/play.js
+++ commands/music/play.js
@@ -6,9 +6,8 @@ const ytdlFilters = {
 	volume: false,
 	type: 'opus',
 };
-const { youtube_v3 } = require('googleapis');
-const { google_key, errorChannelId } = require('../../config.json');
 const { MessageEmbed } = require('discord.js');
+const ytsr = require('ytsr');
 const join = require('./join');
 
 module.exports = {
@@ -18,58 +17,36 @@ module.exports = {
 	aliases: ['p'],
 	guildOnly: true,
 	args: true,
-	async execute(servers, message, args, client) {
+	async execute(servers, message, args) {
 		if (!(await join.execute(servers, message))) return;
 
-		if (ytdl.validateURL(args[0])) {
-			const videoDetails = (await ytdl.getInfo(ytdl.getVideoID(args[0])))
-				.videoDetails;
+		const url = await getUrl(message, args);
 
-			const title = videoDetails.title;
+		const title = (await ytdl.getInfo(url)).videoDetails.title;
 
-			await addToQueue(servers, args[0], title, message);
-		}
-		else {
-			const youtube = new youtube_v3.Youtube({
-				version: 3,
-				auth: google_key,
-			});
-
-			youtube.search.list(
-				{
-					q: args.join(' '),
-					part: 'snippet',
-					fields: 'items(id(videoId))',
-					type: 'video',
-				},
-				async (err, res) => {
-					if (err) {
-						return await client.channels.cache
-							.get(errorChannelId)
-							.send(err, { split: true });
-					}
-
-					try {
-						const id = res.data.items[0].id.videoId;
-
-						const url = 'https://www.youtube.com/watch?v=' + id;
-
-						const videoDetails = (
-							await ytdl.getInfo(res.data.items[0].id.videoId)
-						).videoDetails;
-
-						const title = videoDetails.title;
-
-						await addToQueue(servers, url, title, message);
-					}
-					catch (error) {
-						if (error.name === 'Error [VOICE_PLAY_INTERFACE_BAD_TYPE]') return;
-					}
-				},
-			);
-		}
+		await addToQueue(servers, url, title, message);
 	},
 };
+
+async function getUrl(message, args) {
+	if (ytdl.validateURL(args[0])) {
+		return args[0];
+	}
+
+	const filtered = (await ytsr.getFilters(args.join(' ')))
+		.get('Type')
+		.get('Video');
+
+	const searchResult = (await ytsr(filtered.url, { limit: 1 })).items[0];
+
+	if (!searchResult) {
+		return await message.reply(
+			'Não foi encontrado nenhum vídeo com essa pesquisa',
+		);
+	}
+
+	return searchResult.url;
+}
 
 async function addToQueue(servers, url, title, message) {
 	if (servers[message.guild.id].queue.length > 0) {
@@ -86,44 +63,40 @@ async function addToQueue(servers, url, title, message) {
 		title: title,
 	});
 
-	await playMusic(servers, message);
-}
-
-async function playMusic(servers, message) {
-	if (servers[message.guild.id].playing === false) {
-		servers[message.guild.id].dispatcher = await servers[
-			message.guild.id
-		].connection.play(
-			ytdl(servers[message.guild.id].queue[0].url, ytdlFilters),
-		);
-
-		servers[message.guild.id].dispatcher.on('start', async () => {
-			servers[message.guild.id].playing = true;
-			await embedMessage(
-				message.channel,
-				'Agora tocando',
-				servers[message.guild.id].queue[0].title,
-				servers[message.guild.id].queue[0].url,
-			);
-		});
-
-		servers[message.guild.id].dispatcher.on('finish', async () => {
-			await servers[message.guild.id].queue.shift();
-			servers[message.guild.id].playing = false;
-			if (servers[message.guild.id].queue.length > 0) {
-				await playMusic(servers, message);
-			}
-			else {
-				servers[message.guild.id].dispatcher = null;
-			}
-		});
+	if (!servers[message.guild.id].playing) {
+		await playMusic(servers, message);
 	}
 }
 
-async function embedMessage(channel, title, videoTitle, url) {
-	const embed = new MessageEmbed()
-		.setTitle(title)
-		.setDescription(`[${videoTitle}](${url})`);
+async function playMusic(servers, message) {
+	servers[message.guild.id].dispatcher = await servers[
+		message.guild.id
+	].connection.play(ytdl(servers[message.guild.id].queue[0].url, ytdlFilters));
 
-	channel.send(embed);
+	servers[message.guild.id].dispatcher.on('start', async () => {
+		servers[message.guild.id].playing = true;
+		await embedMessage(
+			message.channel,
+			'Agora tocando',
+			servers[message.guild.id].queue[0].title,
+			servers[message.guild.id].queue[0].url,
+		);
+	});
+
+	servers[message.guild.id].dispatcher.on('finish', async () => {
+		await servers[message.guild.id].queue.shift();
+		servers[message.guild.id].playing = false;
+		if (servers[message.guild.id].queue.length > 0) {
+			await playMusic(servers, message);
+		}
+		else {
+			servers[message.guild.id].dispatcher = null;
+		}
+	});
+}
+
+async function embedMessage(channel, title, videoTitle, url) {
+	await channel.send(
+		new MessageEmbed().setTitle(title).setDescription(`[${videoTitle}](${url})`),
+	);
 }
